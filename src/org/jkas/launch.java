0bf8c49523b2188f951000057ba2484ed1984928
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2023-02-14 15:43:56 -0500

    Server Booster

---
+++ src/org/jkas/launch.java
@@ -0,0 +1,11 @@
+package org.jkas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class launch {
+    public static void main(String[] args) {
+        SpringApplication.run(launch.class);
+    }
+}
--- src/test/java/org/jkas/websitebackend/WebsiteBackEndApplicationTests.java
+++ src/test/java/org/jkas/websitebackend/WebsiteBackEndApplicationTests.java
@@ -1,13 +1,13 @@
-package org.jkas.websitebackend;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WebsiteBackEndApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package org.jkas.websitebackend;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class WebsiteBackEndApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
