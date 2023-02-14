33db9a58dd90bee3e6191653fb024eae7579bde5
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2023-02-14 15:29:36 -0500

    initiali commit

---
+++ src/main/java/org/jkas/websitebackend/WebsiteBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.jkas.websitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsiteBackEndApplication.class, args);
+    }
+
+}
