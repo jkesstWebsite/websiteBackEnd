b5dac249e5ca50e69193e4ed7bcbb5033ebf3dc5
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2023-02-15 12:41:06 -0500

    登陆方法初步完成，优化了代码和一些bug

--- src/org/jkas/User/UserController.java
+++ src/org/jkas/User/UserController.java
@@ -1,10 +1,7 @@
 package org.jkas.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
