c563aec562fe87187a6d6818c1f788e7d3289e74
kimsseTheWolf <ilovemcmrcreeper@outlook.com>
2023-02-15 16:40:06 -0500

    新的返回数据体

---
+++ src/org/jkas/General/NewMessageClass.java
@@ -0,0 +1,24 @@
+package org.jkas.General;
+
+import org.springframework.http.HttpStatus;
+
+import java.net.http.HttpResponse;
+import java.sql.Timestamp;
+
+public class NewMessageClass {
+    public HttpStatus code;
+    public String message;
+    public Timestamp timestamp;
+    public Integer ttl;
+    public Object data;
+
+    // Constructors
+    public NewMessageClass(HttpStatus code, String message){
+        this.code = code;
+        this.message = message;
+        this.timestamp = new Timestamp(System.currentTimeMillis());
+        this.ttl = 1;
+        this.data = new Object();
+    }
+
+}
--- src/org/jkas/User/UserController.java
+++ src/org/jkas/User/UserController.java
@@ -1,11 +1,13 @@
 package org.jkas.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.jkas.General.MessageClass;
+import org.jkas.General.NewMessageClass;
 
 import java.util.HashMap;
 import java.util.List;
@@ -15,10 +17,6 @@ import java.util.Map;
 public class UserController {
     @Autowired
     private JdbcTemplate targetdb = new JdbcTemplate();
-
-    private Object getValue(List<Map<String, Object>> targetList, String targetKey){
-        return targetList.get(0).get(targetKey);
-    }
 
     private Boolean checkUserExistence(String username){
         String sql = String.format("select * from userdb where username='%s'", username);
@@ -39,7 +37,7 @@ public class UserController {
     }
 
     @RequestMapping("/user/register")
-    public MessageClass register(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("email") String email){
+    public NewMessageClass register(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("email") String email){
         // Check whether the username is occupied
         String sql = String.format("select * from userdb where username='%s'", username);
         // Get the result list from the database
@@ -48,62 +46,62 @@ public class UserController {
         // Check if the user is existed. If yes then end the session
         if (userList.size() > 0){
             // means there is an existed user in the database
-            return new MessageClass(false, "The user is already existed");
+            return new NewMessageClass(HttpStatus.NOT_ACCEPTABLE, "The user is already existed");
         }
 
         // if not then continue adding the user into the database
-        sql = String.format("INSERT INTO userdb (username, status, password, email) VALUES ('%s', %s, '%s', '%s')", username, 1, password, email);
+        sql = String.format("INSERT INTO userdb (username, status, password, email, auth) VALUES ('%s', %s, '%s', '%s', %s)", username, 1, password, email, 2);
         int affectRowNum = targetdb.update(sql);
         if (affectRowNum > 0){
             // success
-            return new MessageClass(true, "User created successfully");
+            return new NewMessageClass(HttpStatus.OK, "User created successfully");
         }
         else{
-            return new MessageClass(false, "Technical issue happened when registering user.");
+            return new NewMessageClass(HttpStatus.NOT_ACCEPTABLE, "Technical issue happened when registering user.");
         }
 
     }
 
     @RequestMapping("/user/login")
-    public MessageClass login(@RequestParam("username") String username, @RequestParam("password") String password){
+    public NewMessageClass login(@RequestParam("username") String username, @RequestParam("password") String password){
         // Check whether the user is in the database
         String sql = String.format("select * from userdb where username='%s'", username);
         List<Map<String, Object>> resultList = targetdb.queryForList(sql);
         if (resultList.size() != 0 && resultList.get(0).get("username").equals(username)){
             // check the password for the user
             if (resultList.get(0).get("password").equals(password) && (int)resultList.get(0).get("status") == 1){
-                return new MessageClass(true, "You have been logged in successfully");
+                return new NewMessageClass(HttpStatus.OK, "You have been logged in successfully");
             }
             else{
-                return new MessageClass(false, "Username and password does not match");
+                return new NewMessageClass(HttpStatus.NOT_ACCEPTABLE, "Username and password does not match");
             }
         }
         else{
-            return new MessageClass(false, resultList.toString());
+            return new NewMessageClass(HttpStatus.NOT_ACCEPTABLE, resultList.toString());
         }
     }
 
     @RequestMapping("/user/changeUserStatus")
-    public MessageClass changeUserStatus(@RequestParam("username") String username, @RequestParam("status") Integer status){
+    public NewMessageClass changeUserStatus(@RequestParam("username") String username, @RequestParam("status") Integer status){
         // find this user is in the database
         Boolean result = checkUserExistence(username);
         if (result){
             String sql = String.format("update userdb set status=%s where username='%s'", status, username);
             int processResult = targetdb.update(sql);
             if (processResult == 1){
-                return new MessageClass(true, "User status updated successfully");
+                return new NewMessageClass(HttpStatus.OK, "User status updated successfully");
             }
             else{
-                return new MessageClass(false, "User status modification failed");
+                return new NewMessageClass(HttpStatus.NOT_ACCEPTABLE, "User status modification failed");
             }
         }
         else{
-            return new MessageClass(false, "User does not exist");
+            return new NewMessageClass(HttpStatus.NOT_ACCEPTABLE, "User does not exist");
         }
     }
 
     @RequestMapping("/user/deleteUser")
-    public MessageClass deleteUser(@RequestParam("username") String username){
+    public NewMessageClass deleteUser(@RequestParam("username") String username){
         // check the user existence
         Boolean userIsExist = checkUserExistence(username);
         if (userIsExist){
@@ -111,15 +109,59 @@ public class UserController {
             int affectLineNum = targetdb.update(sql);
             // verify process
             if (affectLineNum != 0){
-                return new MessageClass(true, "User deleted successfully");
+                return new NewMessageClass(HttpStatus.OK, "User deleted successfully");
             }
             else{
-                return new MessageClass(false, "User didn't deleted successfully");
+                return new NewMessageClass(HttpStatus.NOT_ACCEPTABLE, "User didn't deleted successfully");
             }
         }
         else{
             // return that the user does not exist
-            return new MessageClass(false, "User does not exist");
+            return new NewMessageClass(HttpStatus.NOT_ACCEPTABLE, "User does not exist");
+        }
+    }
+
+    @RequestMapping("/user/checkPassword")
+    public NewMessageClass checkUserPassword(@RequestParam("username") String username, @RequestParam("password") String userInputPassword){
+        // check user existence
+        Boolean userIsExisted = checkUserExistence(username);
+        if (userIsExisted){
+            // get the password from the database
+            String sql = String.format("select * from userdb where username='%s'", username);
+            List<Map<String, Object>> result = targetdb.queryForList(sql);
+            // Compare the data and return the result
+            if (userInputPassword.equals(result.get(0).get("password"))){
+                return new NewMessageClass(HttpStatus.OK, "Password is correct");
+            }
+            else{
+                return new NewMessageClass(HttpStatus.NOT_ACCEPTABLE, "Password is incorrect");
+            }
+        }
+        else{
+            return new NewMessageClass(HttpStatus.NOT_ACCEPTABLE, "User does not exist");
+        }
+    }
+
+    @RequestMapping("/user/getUserInfo")
+    public Map<String, Object> returnUserInfo(@RequestParam("username") String username){
+        // check user existence
+        Boolean existResult = checkUserExistence(username);
+        if (existResult){
+            // get the data from the database
+            String sql = String.format("select * from userdb where username='%s'", username);
+            List<Map<String, Object>> result = targetdb.queryForList(sql);
+            result.get(0).remove("password");
+            Map<String, Object> returnData = new HashMap<>();
+            returnData.put("username", result.get(0).get("username"));
+            returnData.put("id", result.get(0).get("id"));
+            returnData.put("email", result.get(0).get("email"));
+            returnData.put("status", result.get(0).get("status"));
+            returnData.put("auth", result.get(0).get("auth"));
+            // return the data
+            return returnData;
+        }
+        else{
+            return null;
         }
     }
 
